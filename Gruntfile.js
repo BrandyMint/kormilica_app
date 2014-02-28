500724d70e45d17b2dc57ca7f2894be27ccba6ab
Sergey Laptev <imperm3r.main@gmail.com>
2014-02-28 17:08:04 +0400

    Quick build fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,7 +197,8 @@ module.exports = function (grunt) {
                         'marionette': '../../app/bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
                         'backbone.wreqr': '../../app/bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
                         'backbone.babysitter': '../../app/bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
-                        'backbone.localStorage': '../../app/bower_components/backbone.localStorage/backbone.localStorage'
+                        'backbone.localStorage': '../../app/bower_components/backbone.localStorage/backbone.localStorage',
+                        'jquery.form-serialize': '../../app/scripts/lib/form-serialize'
                     },
                     // TODO: Figure out how to make sourcemaps work with grunt-usemin
                     // https://github.com/yeoman/grunt-usemin/issues/30
